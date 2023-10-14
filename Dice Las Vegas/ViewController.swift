e128858c0367cf5fe0696eca7d2aab9cdc5d9cdb
Dushanbe Bairambekov <dzhum_7@live.com>
2023-10-14 15:48:32 -0500

    Initial Commit

---
+++ Dice Las Vegas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Las Vegas
+//
+//  Created by Desmond Price on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
